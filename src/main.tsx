4295f192870a3827d275fcebeb0ac89ded1b3315
niawjunior <niawkung@gmail.com>
2023-12-16 15:41:49 +0700

    add g tag

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import TagManager from "react-gtm-module"
 import App from "./App.tsx"
 import "./index.css"
 
-const tagManagerArgs = {
-  gtmId: "GTM-P72XJFM2",
-}
-
-TagManager.initialize(tagManagerArgs)
+// TagManager.initialize(tagManagerArgs)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
